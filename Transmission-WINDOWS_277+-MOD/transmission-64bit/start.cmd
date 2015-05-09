ecaab50d764b7fc3116e9b0281f45aaab5f286f3
tr-cht-fx-242p <tr-cht-fx-242p>
2015-05-09 13:50:49 -0500

    release - daemon - Linux and Mac

--- Transmission-WINDOWS_277+-MOD/transmission-64bit/start.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-
-
-set HOME=%cd%
-set TRANSMISSION_WEB_HOME=%cd%\web
-
-transmission-daemon --pid-file transmission-daemon.pid
--- Transmission-WINDOWS_277+-MOD/transmission-64bit/stop.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-
-set /p PID=<transmission-daemon.pid
-
-taskkill /f /pid %PID%
-del transmission-daemon.pid
