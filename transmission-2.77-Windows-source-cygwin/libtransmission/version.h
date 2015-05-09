a32b6b93a521ffc7ae11626ec702e4ad40f3c4a3
tr-cht-fx-242p <tr-cht-fx-242p>
2015-05-09 06:28:45 -0500

    source and binaries

---
+++ transmission-2.77-Windows-source-cygwin/libtransmission/version.h
@@ -0,0 +1,10 @@
+#define PEERID_PREFIX             "-TR2770-"
+#define USERAGENT_PREFIX          "2.77"
+#define SVN_REVISION              "14485"
+#define SVN_REVISION_NUM          14485
+#define SHORT_VERSION_STRING      "2.77"
+#define LONG_VERSION_STRING       "2.77 (14485)"
+#define VERSION_STRING_INFOPLIST  2.77
+#define MAJOR_VERSION             2
+#define MINOR_VERSION             77
+#define TR_STABLE_RELEASE         1
